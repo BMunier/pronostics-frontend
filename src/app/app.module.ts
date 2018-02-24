4dca533e5032d36c9da18a32f27b9dd2b456ffae
Bastien Munier <munier.bastien@gmail.com>
2018-02-24 13:37:49 +0100

    Ajout de l'écran de login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
 import { AppComponent } from './app.component';
-
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import { UserComponent } from './user/user.component';
+import {AppRoutingModule} from "./core/app.routing.module";
+import { LoginComponent } from './login/login.component';
+import { CustomMaterialModule } from './core/material.module';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    CustomMaterialModule,
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
